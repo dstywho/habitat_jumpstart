68e5a524104e75312e9be18a5bf0d306c0f70a80
Habitat User <hab@ip-10-0-1-78.us-west-2.compute.internal>
2018-05-22 19:57:24 +0000

    bind to core/mogo

--- national-parks/habitat/plan.sh
+++ national-parks/habitat/plan.sh
@@ -4,12 +4,12 @@ pkg_origin=habitat_workshop
 pkg_version=6.3.0
 pkg_maintainer="dstywho@gmail.com>"
 pkg_license=('Apache-2.0')
-pkg_deps=(core/tomcat8 core/jre8)
+pkg_deps=(core/tomcat8 core/jre8 core/mongo-tools)
 pkg_build_deps=(core/jdk8/8u131 core/maven)
 pkg_svc_user="root"
-# pkg_binds=(
-#   [database]="port"
-# )
+pkg_binds=(
+  [database]="port"
+)
 # pkg_exports=(
 #   [port]=tomcat_port
 # )
@@ -29,7 +29,10 @@ do_build()
 
 do_install()
 {
+    # cp /hab/cache/src/national-parks-6.3.0/target/national-parks.war /hab/pkgs/habitat_workshop/national-parks/6.3.0/20180522184801
     cp ${HAB_CACHE_SRC_PATH}/${pkg_dirname}/target/${pkg_name}.war ${PREFIX}/
     mkdir -p ${PREFIX}/config
-    cp $(hab pkg path core/tomcat8)/config/conf_server.xml ${PREFIX}/config
+    # cp /hab/pkgs/core/tomcat8/8.5.9/20170514144202 /hab/pkgs/habitat_workshop/national-parks/6.3.0/2018052218480/config
+    cp $(hab pkg path core/tomcat8)/config/conf_server.xml ${PREFIX}/config/
+    cp ${HAB_CACHE_SRC_PATH}/${pkg_dirname}/data/national-parks.json ${PREFIX}/
 }
